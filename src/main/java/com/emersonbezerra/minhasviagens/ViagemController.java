042195306bbad43843a4869e524b668d43143ec2
Emerson Bezerra <emersondevelops@gmail.com>
2021-11-24 19:47:11 -0300

    Novos arquivos e modificações da aula 03

--- src/main/java/com/emersonbezerra/minhasviagens/ViagemController.java
+++ src/main/java/com/emersonbezerra/minhasviagens/ViagemController.java
@@ -1,20 +1,22 @@
 package com.emersonbezerra.minhasviagens;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/viagem")
 public class ViagemController {
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Viagem> mostarViagem() {
 
         Viagem viagem = new Viagem("João Pessoa", "Maldivas");
 
-        return ResponseEntity.ok().body(viagem);
+        return ResponseEntity.status(HttpStatus.OK).body(viagem);
     }
 }
